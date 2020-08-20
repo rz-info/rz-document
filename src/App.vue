8cc92d5484c81144992f885b39c37fcb63e0ed25
zhangqin <15877656725@163.com>
2020-08-20 15:24:20 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<!--
+ * @Author: Zhang Qin
+ * @Date: 2020-08-19 12:07:45
+ * @LastEditTime: 2020-08-20 15:23:58
+ * @LastEditors: 
+ * @Version: 
+ * @Description: 
+ * @param: 
+ * @FilePath: \v2.0.1d:\项目\rz-document.github.io\src\App.vue
+-->
 <template>
   <div id="app">
     <router-view />
@@ -5,41 +15,41 @@
 </template>
 
 <script>
-export default {
-  name: "App"
-};
+  export default {
+    name: "App"
+  };
 </script>
 <style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
 
-#app {
-  height: 100%;
-}
+  #app {
+    height: 100%;
+  }
 
-code,
-pre {
-  font-family: "Roboto Mono", Monaco, courier, monospace;
-  font-size: 0.85em;
-  background-color: #f8f8f8;
-  -webkit-font-smoothing: initial;
-  -moz-osx-font-smoothing: initial;
-}
+  code,
+  pre {
+    font-family: "Roboto Mono", Monaco, courier, monospace;
+    font-size: 0.85em;
+    background-color: #f8f8f8;
+    -webkit-font-smoothing: initial;
+    -moz-osx-font-smoothing: initial;
+  }
 
-pre {
-  padding: 3px 5px;
-}
+  pre {
+    padding: 3px 5px;
+  }
 
-code:not([class]) {
-  display: inline-block;
-  color: #d63200;
-  padding: 3px 5px;
-  margin: 0 2px;
-  border-radius: 2px;
-  white-space: nowrap;
-}
+  code:not([class]) {
+    display: inline-block;
+    color: #d63200;
+    padding: 3px 5px;
+    margin: 0 2px;
+    border-radius: 2px;
+    white-space: nowrap;
+  }
 </style>
