69ff8b8bf3fbe6144365702c17c622a1e8c44657
zhangqin <15877656725@163.com>
2020-10-11 16:51:18 +0800

    表格数据点击index,查看修改记录

--- src/views/components/table.vue
+++ src/views/components/table.vue
@@ -38,6 +38,33 @@
         <exp3 />
       </template>
     </zExample>
+		<zExample title="示例四">
+		  <h4>使用slot自定义列表内容2</h4>
+			<el-row style="margin-top:30px" class="table_his">
+				<commonTable :tableMap="example4.tableMap" :tableData="example4.tableData" @editEject="editEject" :tableConfig="example4.tableConfig">
+					<template v-slot:btn="{ rowData, template, scope }">
+						<template v-for="(item,key) in template">
+							<div v-if="key == 'img'" :key="key">
+								 <span>{{scope.$index+1}}&emsp;</span>
+								<img style="margin-left:20px;" v-if="rowData.modifyNumber" src="@/views/classes/icon/sanjiao.png" @click.stop="selectEditHistorical(scope.$index,rowData)" alt="">
+								<div class="historyInvoice" v-show="example4.showKey[scope.$index]" >
+									<div v-for="item in example4.LedgerHistory" :key="item.id" >
+										<div class="list">
+											<span class="date">{{item.updatedAt}}</span>
+											<p class="text">{{item.modifyContent}}</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</template>
+					</template>
+				</commonTable>
+			</el-row>
+		  <template v-slot:document>
+				<!-- 放md文件 -->
+				<exp4 />
+		  </template>
+		</zExample>
     <testMD />
   </zTitle>
 </template>
@@ -46,6 +73,7 @@
   import exp1 from "@/assets/markdown/components/table/exp1.md";
   import exp2 from "@/assets/markdown/components/table/exp2.md";
   import exp3 from "@/assets/markdown/components/table/exp3.md";
+  import exp4 from "@/assets/markdown/components/table/exp4.md";
   
   import commonTable from "@/components/Common/table.vue";
 
@@ -254,19 +282,156 @@
     }],
   }
 
+  // 示例4代码
+  const mixin4 = {
+    data: () => {
+      return {
+        example4,
+      }
+    },
+		methods:{
+			selectEditHistorical(key,data){
+				this.hideTips();
+				this.$nextTick(()=>{
+					this.$set(this.example4.showKey,key,true)
+  				// 这里可以请求接口获取参数
+// 					 let dto = {
+// 						id:data.id,
+// 						status:data.status
+// 					 }
+// 					 findPayLedgerHistory(dto).then(res => {
+// 						this.LedgerHistory = res.data;
+// 					 })
+					this.example4.LedgerHistory=[{updatedAt:"000000",modifyContent:"内容999999999999"},{updatedAt:"000000",modifyContent:"内容999999999999"}]
+				});
+					
+			},
+			hideTips() {
+				for(let i in this.example4.showKey) {
+					this.example4.showKey[i] = false;
+				}
+			}
+		},
+		mounted(){
+			document.addEventListener('click',this.hideTips);
+		},
+		destroy(){
+			document.removeEventListener('click',this.hideTips);
+		}
+	}
+// 示例4 data
+  const example4 = {
+		showKey:{},//标记当前点击的列表
+		LedgerHistory:[],
+    tableData: {
+      data: [{
+				modifyNumber:null,
+				content:"内容",
+				remark:"备注",
+				id:"1",
+				status:"aa"
+				},
+				{
+				modifyNumber:2,
+				content:"内容",
+				remark:"备注",
+				id:"2",
+				status:"bb"
+				}
+      ],
+      total: 2,
+      page: 1,
+      pageSize: 2
+    },
+		tableConfig : { // 其他配置, 选填
+      hideIndex: true // 隐藏序号列, 默认false
+    },
+    tableMap: [
+			{
+        name: '序号',
+        prop:"index",
+        width: 80, // 宽度, 选填
+        template: {
+          div: {
+            event: 'click'
+          },
+          img:{
+            event: 'click'
+          }
+        }
+      },
+      {
+        name: '内容',
+        prop: 'content',
+      },
+			{
+			  name: '备注',
+			  prop: 'remark',
+			},
+      {
+        name: '查看',
+        prop: 'authFileName',
+        width: 200, // 宽度, 选填
+        template: { // 其他组件写在 templat 里, 选填
+          btn: [{
+            label: '附件', // 按钮文本
+            style: 'background:#1DB2A0;border:none',
+            event: 'invoiceAttachment' // 按钮事件(需在父级定义同名函数并当做自定义事件传入表格组件)
+          }]
+        }
+      }
+    ]
+  }
   export default {
     components: {
       testMD,
       exp1,
       exp2,
       exp3,
+			exp4,
       commonTable,
     },
-    mixins: [mixin1, mixin2, mixin3],
+    mixins: [mixin1, mixin2, mixin3,mixin4]
   };
 </script>
 <style lang="scss" scoped>
   .wrapper {
     width: calc(100% - 1px);
+  }
+	.historyInvoice{
+    position: absolute;
+    left: 75px;
+    background: #EAF1FF;
+    width: 500px;
+    z-index: 9999;
+    box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    padding: 20px;
+  }
+  .historyInvoice .list{
+    text-align: left;
+    margin-bottom: 20px;
+  }
+  .historyInvoice .date{
+    font-size: 14px;
+    font-family: Microsoft YaHei;
+    font-weight: 400;
+    color: #666666;
+    line-height: 10px;
+  }
+  .historyInvoice .text{
+    font-size: 14px;
+    font-family: Microsoft YaHei;
+    font-weight: 400;
+    color: #1A1A1A;
+    line-height: 10px;
+  }
+</style>
+<style lang="css" scoped>
+  .table_his>>>.el-table__body-wrapper{
+    overflow: visible;
+  }
+  .table_his>>>.el-table{
+    overflow: visible;
   }
 </style>
